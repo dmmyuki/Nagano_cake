71110af27a6bc84b8b5249e55886952b2855d151
dmmyuki <104970837+dmmyuki@users.noreply.github.com>
2022-06-25 14:04:48 +0000

    [Create]注文履歴一覧・詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources:genres, only:[:index, :create, :edit, :update]
     resources:items, only:[:new, :index, :show, :edit, :create, :update]
     resources:customers, only:[:index, :show, :edit, :update]
+    resources:orders, only:[:show, :update]
   end
 
   namespace :public do

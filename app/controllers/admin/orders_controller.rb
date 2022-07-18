1460de33e81ef481e0b526bf0f9585f3d0938e3c
dmmyuki <104970837+dmmyuki@users.noreply.github.com>
2022-07-18 14:09:51 +0000

    [Add]ヘッダー、色々修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,4 +3,9 @@ class Admin::OrdersController < ApplicationController
     @order=Order.find(params[:id])
     @order_details=@order.order_details
   end
+  
+  private
+  def order_params
+    params.require(:order).permit(:destination_postal_code, :destination_address, :destination_name, :payment_method)
+  end
 end
